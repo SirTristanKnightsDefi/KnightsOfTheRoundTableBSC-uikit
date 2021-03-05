977cf5e7428451239dede4efef721bc77058ba04
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-05 06:30:05 -0500

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,13 +1,13 @@
 import { Colors } from "./types";
 
 export const baseColors = {
-  failure: "#ff738e",
-  primary: "#5DC4D9",
-  primaryBright: "#5DC4D9",
-  primaryDark: "#5DC4D9",
-  secondary: "#5DC4D9",
-  success: "#33CA7F",
-  warning: "#fea192",
+  failure: "#fa113e",
+  primary: "#ccc72d",
+  primaryBright: "#ccc72d",
+  primaryDark: "#4e23a6",
+  secondary: "#ccc72d",
+  success: "#15bd69",
+  warning: "#fa113e",
 };
 
 export const brandColors = {
@@ -25,7 +25,7 @@ export const lightColors: Colors = {
   tertiary: "#EFF4F5",
   text: "#5f7888",
   textDisabled: "#BDC2C4",
-  textSubtle: "#5EC3DA",
+  textSubtle: "#3868b5",
   borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
@@ -36,7 +36,7 @@ export const lightColors: Colors = {
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#9A6AFF",
+  secondary: "#703ddb",
   background: "#100C18",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
