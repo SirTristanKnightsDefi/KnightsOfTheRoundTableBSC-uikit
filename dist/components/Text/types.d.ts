5b91b2af658e9c46a143314b22389571b394a82a
Noah Ronnebergg <noah.ronneberg@gmail.com>
2021-03-08 20:24:38 -0500

    Revert "Try night mode again."
    This reverts commit 0089bb295da94f5304a89da46c468cba0464c69d.

--- dist/components/Text/types.d.ts
+++ dist/components/Text/types.d.ts
@@ -1,5 +1,4 @@
 import { SpaceProps } from "styled-system";
-import { InputHTMLAttributes } from "react";
 export interface TextProps extends SpaceProps {
     color?: string;
     fontSize?: string;
@@ -7,31 +6,3 @@ export interface TextProps extends SpaceProps {
     small?: boolean;
     textTransform?: "uppercase" | "lowercase" | "capitalize";
 }
-export declare type ToggleTheme = {
-    handleBackground: string;
-};
-export declare const scales: {
-    readonly SM: "sm";
-    readonly MD: "md";
-};
-export declare type Scales = typeof scales[keyof typeof scales];
-export interface ToggleProps extends InputHTMLAttributes<HTMLInputElement> {
-    scale?: Scales;
-    checked?: boolean;
-}
-export interface HandleProps {
-    scale: Scales;
-}
-export interface InputProps {
-    scale: Scales;
-}
-export declare const scaleKeys: {
-    readonly handleHeight: "handleHeight";
-    readonly handleWidth: "handleWidth";
-    readonly handleLeft: "handleLeft";
-    readonly handleTop: "handleTop";
-    readonly checkedLeft: "checkedLeft";
-    readonly toggleHeight: "toggleHeight";
-    readonly toggleWidth: "toggleWidth";
-};
-export declare type ScaleKeys = typeof scaleKeys[keyof typeof scaleKeys];
