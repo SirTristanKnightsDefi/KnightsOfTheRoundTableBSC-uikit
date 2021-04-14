251c44b1b3f630f1b670ad2ee6f7aa3991cc58ea
Sir Tristan <sirtris@knightsdefi.com>
2021-04-13 22:16:45 -0400

    update card box shadow to purple

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -3231,21 +3231,21 @@ var dark$6 = {
 
 var light$5 = {
     background: lightColors.card,
-    boxShadow: "0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)",
+    boxShadow: "0px 2px 12px -8px rgba(112, 61, 219, 0.1), 0px 1px 1px rgba(112, 61, 219, 0.05)",
     boxShadowActive: shadows.active,
     boxShadowSuccess: shadows.success,
     boxShadowWarning: shadows.warning,
     cardHeaderBackground: "linear-gradient(111.68deg, #F2ECF2 0%, #E8F2F6 100%)",
-    dropShadow: "drop-shadow(0px 1px 4px rgba(25, 19, 38, 0.15))",
+    dropShadow: "drop-shadow(0px 1px 4px rgba(112, 61, 219, 0.15))",
 };
 var dark$5 = {
     background: darkColors.card,
-    boxShadow: "0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)",
+    boxShadow: "0px 2px 12px -8px rgba(112, 61, 219, 0.1), 0px 1px 1px rgba(112, 61, 219, 0.05)",
     boxShadowActive: shadows.active,
     boxShadowSuccess: shadows.success,
     boxShadowWarning: shadows.warning,
     cardHeaderBackground: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
-    dropShadow: "drop-shadow(0px 1px 4px rgba(25, 19, 38, 0.15))",
+    dropShadow: "drop-shadow(0px 1px 4px rgba(112, 61, 219, 0.15))",
 };
 
 var light$4 = {
